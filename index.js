5d29cbd5a2e4b81c07c83b2c7d9b69e36cb44175
Neha Kumari <96180294+neha-kum@users.noreply.github.com>
2022-11-04 16:31:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+})
+
+function getFeedHtml(){
+    
+  let feedHtml= ``
+
+    tweetsData.forEach(function(tweet){
+      
+      feedHtml += `
+    
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                <i class="fa-regular fa-comment-dots"></i>
+                  ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                <i class="fa-solid fa-heart"></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                <i class="fa-solid fa-retweet"></i>
+                     ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>`
+    })
+return feedHtml
+
+  }
+
+function render(){
+document.getElementById('feed').innerHTML= getFeedHtml()
+}
+
+render()
